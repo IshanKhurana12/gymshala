3e3c09b296362555cdc60c5d82f917deb75fee90
Ishan Khurana <ishankhurana@Ishans-MacBook-Air.local>
2025-04-04 06:03:30 +0530

    added footer

--- src/main.jsx
+++ src/main.jsx
@@ -9,23 +9,26 @@ import Update from './Update.jsx';
 import Delete from './Delete.jsx';
 import Login from './Login.jsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { enqueueSnackbar } from 'notistack';
 
 createRoot(document.getElementById('root')).render(
-
-    <Router>
-      <Navbar />
-      <Routes>
-      
-        <Route path="/login" element={<Login />} />
-
-        <Route path="/" element={<App />} />
-        <Route path="/add" element={<Add />} />
-        <Route path="/update" element={<Update />} />
-        <Route path="/delete" element={<Delete />} />
-
-
-        <Route path="*" element={<NotFound />} />
-
-      </Routes>
-    </Router>
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<App />} />
+      <Route path="/add" element={<Add />} />
+      <Route path="/update" element={<Update />} />
+      <Route path="/delete" element={<Delete />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    <footer className="bg-gray-800 text-white text-center py-4">
+      Made with <span className="text-red-500">♥</span> by Ishan Khurana | Contact: 9871335748
+    </footer>
+    <Toaster position="top-right" />
+  </Router>
 );
+
+// Example usage of enqueueSnackbar for notifications
+enqueueSnackbar('Welcome to GymShala!', { variant: 'success' });
